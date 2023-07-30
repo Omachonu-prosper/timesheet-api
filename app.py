db632bb54e3063977bfbedc019be1e6688156398
Omachonu Prosper <omachonucodes@gmail.com>
2023-07-30 18:55:27 +0100

    Model data and prototype routes to fetch report

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, request, jsonify
+from user_model import users
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-	return 'Hello World'
+@app.route('/view/reports/all')
+def get_all_reports():
+	data = []
+	for user in users:
+		data.append(user['reports'])
+	response = {
+		"message": "Fetched report data successfully",
+		"status": True,
+		"data": data
+	}
+	return jsonify(response)
 
+
+@app.route('/')
+def index():
+	return "Timesheet API V-0.0.1", 200
 
 if __name__ == '__main__':
 	app.run(debug=True)
