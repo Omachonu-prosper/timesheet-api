45a049ec128885004857db9cc693e94d5d4ae737
Omachonu Prosper <omachonucodes@gmail.com>
2023-08-10 22:39:49 +0100

    Clean up report capturing route

--- app.py
+++ app.py
@@ -4,31 +4,14 @@ from pymongo import MongoClient
 from bson import ObjectId
 from uuid import uuid1
 
+from app_logic.validate_record_report import validate_record_report
+from app_logic.validate_report_date import validate_report_date
+
 
 app = Flask(__name__)
 client = MongoClient("mongodb://localhost:27017/")
 db = client['worksheet']
 col = db['users']
-
-
-def get_day_of_week(date):
-	weekday = date.weekday()
-
-	match weekday:
-		case 0:
-			return "Monday"
-		case 1:
-			return "Tuesday"
-		case 2:
-			return "Wednesday"
-		case 3:
-			return "Thursday"
-		case 4:
-			return "Friday"
-		case 5:
-			return "Saturday"
-		case 6:
-			return "Sunday"
 
 
 @app.route('/view/reports/all')
@@ -52,61 +35,36 @@ def get_all_reports():
 
 @app.route('/record/report/<string:user_id>', methods=['POST'])
 def record_report(user_id):
-	date = request.json.get('date')
-	project = request.json.get('project')
-	task = request.json.get('task')
-	status = request.json.get('status')
-	link = request.json.get('link')
-	duration = request.json.get('duration')
+	response = validate_record_report(request.json)
+	if response.get('error'):
+		return response['message'], response['error-code']
 
-	if not date or not project or not task or not status or not link or not duration:
-		return 'Missing required parameter', 400
-
-	try:
-		date = datetime.strptime(date, "%Y-%m-%d")
-		week_start = date - timedelta(days=date.weekday())
-		created_at = datetime.now()
-		day_of_week = get_day_of_week(date)
+	dates = validate_report_date(response['date'])
+	if dates.get('error'):
+		return dates['message'], dates['error-code']
+	# Copies the values of dates to the response dictionary
+	response.update(dates)
 		
-		now = datetime.now()
-		today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-		current_week = today - timedelta(days=today.weekday())
-	except:
-		return 'Date is not a valid datetime format', 400
-
-
-	if current_week != week_start:
-		# Once it is past 8:00am on Monday do not accept submissions for any other week apart from the current week
-		# today.weekday == 0 (shows that it is a monday)
-		# now.hour < 8 (the time is not yet 8:00am)
-		# week_start == current_week - timedelta(weeks=1) 
-		# 	(shows that the report we are trying to submit is a report of the past week
-		# 	and so can be allowed since the time is not yet 8:00am)
-		if today.weekday() == 0 and now.hour < 8 and week_start == current_week - timedelta(weeks=1):
-			pass
-		else:
-			return "Failed to record report: submission window exceeded", 403
-
-	payload = {
-		"id": str(uuid1()),
-		"date": date,
-		"project": project,
-		"task": task,
-		"link": link,
-		"status": status,
-		"duration": duration,
-		"day-of-week": day_of_week,
-		"week-start": week_start,
-		"created-at": created_at
-	}
-
 	# return 'Failed to record report: report already recorded', 409
 	report_exists = col.find({
 		'_id': ObjectId(user_id),
-		'reports.date': date
+		'reports.date': response['date']
 		}, {'_id': 1})
 	if list(report_exists):
 		return 'Failed to record report: report already recorded', 409
+
+	payload = {
+		"id": str(uuid1()),
+		"date": response['date'],
+		"project": response['project'],
+		"task": response['task'],
+		"link": response['link'],
+		"status": response['status'],
+		"duration": response['duration'],
+		"day-of-week": response['day-of-week'],
+		"week-start": response['week-start'],
+		"created-at": response['created-at']
+	}
 
 	insert = col.update_one({'_id': ObjectId(user_id)}, {'$push': {'reports': payload}})
 	if not insert.matched_count:
