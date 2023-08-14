de7208545993de7e82edc6f82674ee9754f8c60a
Omachonu Prosper <omachonucodes@gmail.com>
2023-08-14 01:28:08 +0100

    Bug fix: work on users without a report subcollection

--- app_logic/format_data.py
+++ app_logic/format_data.py
@@ -3,12 +3,16 @@ def format_data(data, current_week):
     # Format the data in a usable way
     for elem in data:
         formated_data[elem['username']] = {}
-        formated_data[elem['username']]['monday'] = elem['reports'][current_week].get('monday')
-        formated_data[elem['username']]['tuesday'] = elem['reports'][current_week].get('tuesday')
-        formated_data[elem['username']]['wednesday'] = elem['reports'][current_week].get('wednesday')
-        formated_data[elem['username']]['thursday'] = elem['reports'][current_week].get('thursday')
-        formated_data[elem['username']]['friday'] = elem['reports'][current_week].get('friday')
-        formated_data[elem['username']]['saturday'] = elem['reports'][current_week].get('saturday')
-        formated_data[elem['username']]['sunday'] = elem['reports'][current_week].get('sunday')
-		
+        reports = elem.get('reports')
+        if reports:
+            current_week_reports = reports.get(current_week)
+            if current_week_reports:
+                formated_data[elem['username']]['monday'] = current_week_reports.get('monday')
+                formated_data[elem['username']]['tuesday'] = current_week_reports.get('tuesday')
+                formated_data[elem['username']]['wednesday'] = current_week_reports.get('wednesday')
+                formated_data[elem['username']]['thursday'] = current_week_reports.get('thursday')
+                formated_data[elem['username']]['friday'] = current_week_reports.get('friday')
+                formated_data[elem['username']]['saturday'] = current_week_reports.get('saturday')
+                formated_data[elem['username']]['sunday'] = current_week_reports.get('sunday')
+
     return formated_data
