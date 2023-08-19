16322cb97af4c367b26ae64c1421cc8dcdb1915b
Omachonu Prosper <omachonucodes@gmail.com>
2023-08-19 18:19:53 +0100

    Modularize the database connection login

---
+++ app_logic/connect_to_db.py
@@ -0,0 +1,14 @@
+import os
+from pymongo import MongoClient
+
+# Pymongo instantiation
+app_environment = os.environ.get('APP_ENVIRONMENT', 'development')
+if app_environment.lower() == 'production':
+	db_uri = os.environ.get('DB_URI')
+else:
+	db_uri = 'mongodb://localhost:27017/'
+
+client = MongoClient(db_uri)
+db = client['worksheet']
+users = db['users']
+admins = db['admins']
