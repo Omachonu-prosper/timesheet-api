7d20929c7da78d8e54b4d5df240cfd03fc0eb966
Omachonu Prosper <omachonucodes@gmail.com>
2023-09-07 21:53:22 +0100

    Create helper function to create database user for testing

--- tests/helpers/__init__.py
+++ tests/helpers/__init__.py
@@ -1,5 +1,6 @@
 import os
 
+from app import bcrypt
 from dotenv import load_dotenv
 from bson import ObjectId
 from app_logic.connect_to_db import users
@@ -7,13 +8,28 @@ from app_logic.connect_to_db import users
 load_dotenv()
 API_KEY = os.environ.get('API_KEY', None)
 
-# Create a helper to create users (signup)
-def create_user(username=None, email=None, password=None):
-    pass
+# Returns a user object from the database which can be used in test cases
+def return_db_user():
+    user_obj = {
+        'username': 'Test-user',
+        'firstname': 'Test',
+        'lastname': 'User',
+        'email': 'testuser@example.com',
+        'password': bcrypt.generate_password_hash('password')
+    }
+    user = users.insert_one(user_obj)
+    if user.acknowledged:
+        user_obj['_id'] = str(user.inserted_id)
+        user_obj['password'] = 'password'
+        return user_obj
+    else:
+        return False
+        
 
 # Create a helper to test for api-keys and access-tokens for endpoints
 def api_key_required():
     pass
+
 
 # Delete users from db after creation
 def delete_user(user_id):
