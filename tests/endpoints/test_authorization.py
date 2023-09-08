0c501495e8c42279048136745d947272713562bc
Omachonu Prosper <omachonucodes@gmail.com>
2023-09-08 18:04:33 +0100

    Scafolding for test authorization

---
+++ tests/endpoints/test_authorization.py
@@ -0,0 +1,45 @@
+import unittest
+import requests
+
+"""
+This module contains a series of tests, testing that all protected
+endpoints can not be accessed without proper credentials
+
+Test cases for TestAuthorization
+- Send requests without API keys
+- Send requests with wrong API keys
+"""
+class TestAuthorization(unittest.TestCase):
+    def make_request(url, method):
+        pass
+    
+    def test_index(self):
+        pass
+
+
+    def test_login(self):
+        pass
+
+
+    def test_admin_login(self):
+        pass
+
+
+    def test_signup(self):
+        pass
+
+
+    def test_get_all_reports(self):
+        pass
+
+
+    def test_get_user_reports(self):
+        pass
+
+
+    def test_record_report_post(selt):
+        pass
+
+
+    def test_record_report_put(self):
+        pass
