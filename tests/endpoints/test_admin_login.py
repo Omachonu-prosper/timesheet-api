c8bb0cebe4a06067767b82b614507e74737fc792
Omachonu Prosper <omachonucodes@gmail.com>
2023-09-09 17:38:54 +0100

    Scafolding for admin login tests

---
+++ tests/endpoints/test_admin_login.py
@@ -0,0 +1,23 @@
+import unittest
+import requests
+import helpers
+import json
+
+
+class TestAdminLogin(unittest.TestCase):
+    """
+    Test class to test admin login '/admin/login'
+
+    Test cases
+    - Incomplete payload
+    - Incorrect credentials
+    - Correct credentials
+    """
+    def test_incomplete_payload(self):
+        pass
+
+    def test_incorrect_credentials(self):
+        pass
+
+    def test_correct_credentials(self):
+        pass
