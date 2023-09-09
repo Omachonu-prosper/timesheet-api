7d553dd2541e8fc43ec3b2ec2494e9c6f78ce716
Omachonu Prosper <omachonucodes@gmail.com>
2023-09-09 17:49:58 +0100

    Test admin login with incomplete payload

--- tests/endpoints/test_admin_login.py
+++ tests/endpoints/test_admin_login.py
@@ -13,8 +13,48 @@ class TestAdminLogin(unittest.TestCase):
     - Incorrect credentials
     - Correct credentials
     """
+
+    url = 'http://127.0.0.1:5000/admin/login'
+    headers = {
+        'x-api-key': helpers.API_KEY,
+        'Content-Type': 'application/json'
+    }
+
     def test_incomplete_payload(self):
-        pass
+        """
+        Workflow
+        - Send request with only username as payload
+        - Send request with only password as payload
+        - Test that the response is as expected
+        """
+        
+        req_email = requests.post(
+            url=self.url,
+            headers=self.headers,
+            data=json.dumps({
+                'username': 'admin-username'
+            })
+        )
+        req_password = requests.post(
+            url=self.url,
+            headers=self.headers,
+            data=json.dumps({
+                'password': 'admin-password'
+            })
+        )
+
+        self.assertEqual(
+            req_email.json()['message'],
+            'Missing required parameter'
+        )
+        self.assertEqual(req_email.status_code, 400)
+        self.assertFalse(req_email.json()['status'])
+        self.assertEqual(
+            req_password.json()['message'],
+            'Missing required parameter'
+        )
+        self.assertEqual(req_password.status_code, 400)
+        self.assertFalse(req_password.json()['status'])
 
     def test_incorrect_credentials(self):
         pass
